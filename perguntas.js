71e6181c4559630c08c13dd0c2c7269bff11b34b
JoseRamo859 <augusto.ramos.jose@escola.pr.gov.br>
2025-07-28 08:13:03 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,53 @@
 criaCartao(
     'Programação',
+    'Quem foi a primeira pessoa a viajar no Espaço?
+?',
+    'Yuri Gagarin.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest.'
+'
+)
+
+criaCartao(
+    'Programação',
+    'Que país tem o formato de uma bota?',
+    'Itália.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison.'
+)
+    
+    
+    criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'criaCartao(
+    'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
@@ -20,4 +68,3 @@ criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
-)
